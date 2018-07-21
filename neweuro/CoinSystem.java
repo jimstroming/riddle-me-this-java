31e5aa47ec4d07503a0bff8977e6f2c9451c5f3e
Jim Stroming <jim.stroming@ibm.com>
2018-07-21 18:10:44 -0500

    have the List<Integer> working

--- neweuro/CoinSystem.java
+++ neweuro/CoinSystem.java
@@ -2,11 +2,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.IntStream;
 import java.util.List;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Collections;
+
+
 
 public class CoinSystem {
 
