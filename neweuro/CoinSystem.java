48475d5c895472ca8fc392ce7a5faecb60521685
Jim Stroming <jim.stroming@ibm.com>
2018-07-21 17:22:15 -0500

    order from largest to smallest coin

--- neweuro/CoinSystem.java
+++ neweuro/CoinSystem.java
@@ -6,6 +6,7 @@ import java.util.stream.Stream;
 import java.util.List;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Collections;
 
 public class CoinSystem {
 
@@ -17,13 +18,20 @@ public class CoinSystem {
         public CoinSystem(Integer coin1, Integer coin2, Integer coin3, Integer coin4) {
             List<Integer> unsortedCoinSystem = Arrays.asList(coin1, coin2, coin3, coin4);
             unsortedCoinSystem.sort(Comparator.naturalOrder());
+            Collections.reverse(unsortedCoinSystem);
             coinSystem = unsortedCoinSystem;
             coinSystem.forEach(System.out::println);
         }
 
+
+        public Integer calculateChange(Integer salePrice) {
+            final Integer change = 0;
+            return change;
+        }
+
         public static void main(String[] args) {
 
-            CoinSystem coinSystem = new CoinSystem(1,7,3,4);
+            final CoinSystem coinSystem = new CoinSystem(1,7,3,4);
 
         }
         
