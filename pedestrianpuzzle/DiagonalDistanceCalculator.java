c642105fa29a28850fc83a3cd727b08afdcd20af
Jim Stroming <jim.stroming@ibm.com>
2020-02-15 17:21:01 -0600

    fix bug in DiagonalDistanceCalculator

--- pedestrianpuzzle/DiagonalDistanceCalculator.java
+++ pedestrianpuzzle/DiagonalDistanceCalculator.java
@@ -15,17 +15,49 @@ class DiagonalDistanceCalculator implements DistanceCalculator {
     return recursiveCalculateDistance(workerHomex, workerHomey) * sqrt2/2;
   }
  
+
+  private Point calculateNextPoint(final double currentx, final double currenty) {
+
+    final double nextx;
+    final double nexty; 
+
+    if ((currentx == townHall.x) && (currenty == townHall.y)) {
+      nextx = currentx;
+      nexty = currenty;
+    }
+    else if ((currentx >= townHall.x) && (currenty >= townHall.y)) {
+      nextx = currentx - 0.5;
+      nexty = currenty - 0.5;
+    }
+    else if ((currentx <= townHall.x) && (currenty <= townHall.y)) {
+      nextx = currentx + 0.5;
+      nexty = currenty + 0.5;
+    }
+    else if (currentx >= townHall.x) {
+      nextx = currentx - 0.5;
+      nexty = currenty + 0.5;
+    }
+    else {
+      nextx = currentx + 0.5;
+      nexty = currenty - 0.5;
+    }
+
+    return new Point(currentx, currenty);
+  }
+
   private double recursiveCalculateDistance(final double workerHomex, final double workerHomey) {
+    // System.out.println("x = " + workerHomex + " ; y = " + workerHomey 
+    //                  + " goalx = " + townHall.x + " goaly = " + townHall.y);
     if ((workerHomex == townHall.x) && (workerHomey == townHall.y)) return 0;
     if ((workerHomex >= townHall.x) && (workerHomey >= townHall.y)) {
       return 1 + recursiveCalculateDistance(workerHomex-0.5, workerHomey-0.5);
     }
-    if ((workerHomex < townHall.x) && (workerHomey < townHall.y)) {
+    if ((workerHomex <= townHall.x) && (workerHomey <= townHall.y)) {
       return 1 + recursiveCalculateDistance(workerHomex+0.5, workerHomey+0.5);
     }
-    if (workerHomex > townHall.x) {
-      return 1 + recursiveCalculateDistance(workerHomex-0.5, workerHomey-0.5);
+    if (workerHomex >= townHall.x) {
+      return 1 + recursiveCalculateDistance(workerHomex-0.5, workerHomey+0.5);
     }
-    return 1 + recursiveCalculateDistance(workerHomex+0.5, workerHomey+0.5);
+    return 1 + recursiveCalculateDistance(workerHomex+0.5, workerHomey-0.5);
   }
 }
